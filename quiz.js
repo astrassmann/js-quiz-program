70649133e2434a22e72e654220224dea84541ede
astrassmann <strassmann@mac-a45e60f0ce47.dhcp.wustl.edu>
2016-04-13 18:11:38 -0500

    Added questions and answers

--- quiz.js
+++ quiz.js
@@ -1,13 +1,52 @@
 // variables
 var correct = 0;
+var rank = "";
 
-// prompt
-var answer1 = prompt("ask a question...");
+// prompt for 5 questions
+var answer1 = prompt("Who is our school named after (last name)?");
+var answer2 = prompt("What room numberare we in?");
+var answer3 = prompt("What website do we use to code?");
+var answer4 = prompt("What state are we in?");
+var answer5 = prompt("What is your favorite class?");
 
-// if statement
-if ( answer1.toUpperCase() === 'MAGIC' ) {
+
+// if statements to count how many correct answers
+if ( answer1.toUpperCase() === 'WASHINGTON' ) {
+ correct += 1;
+}
+if ( answer2.toUpperCase() === '218' ) {
+ correct += 1;
+}
+if ( answer3.toUpperCase() === 'GITHUB' ) {
+ correct += 1;
+}
+if ( answer4.toUpperCase() === 'MISSOURI' ) {
+ correct +=1;
+}
+if ( answer5.toUpperCase() === 'WEB DEVELOPMENT' ) {
  correct += 1;
 }
 
+//if statements to rank people
+if (correct === 5){
+	rank = "gold";
+}
+if (correct === 4){
+	rank = "silver";
+}
+if (correct === 3){
+	rank = "bronze";
+}
+
 // output results
-document.write("<p>write something to page...</p>");
+if (correct >0){
+	document.write("<p> congrats! you got " + correct + " out of 5 questions right!</p>");
+}
+
+if (correct === 0){
+	document.write("<p> you didn't get any questions right, but try again by refreshing this page!</p>");
+}
+
+if (correct >2){
+	document.write("<p>You got the " + rank + " medal!</p>");
+}
